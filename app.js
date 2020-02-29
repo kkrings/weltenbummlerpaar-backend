15b0e97fea4cbb8406e649efc3703b30dba0d951
Kai Krings <kai.krings@posteo.de>
2020-02-29 15:24:25 -0600

    Try to better separate servide-side end points from front-end routing.

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/entries', entryRouter);
-app.use('/images', imageRouter);
+app.use('/db/entries', entryRouter);
+app.use('/db/images', imageRouter);
 
 module.exports = app;
