24572c6af8f7a695fddd0ca4e5143baa7f50c09f
Kai Krings <kai.krings@posteo.de>
2020-03-01 12:13:54 -0600

    Install and enable helmet.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
+const helmet = require('helmet');
 
 const entryRouter = require('./routes/entries');
 const imageRouter = require('./routes/images');
@@ -11,6 +12,7 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(helmet());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
