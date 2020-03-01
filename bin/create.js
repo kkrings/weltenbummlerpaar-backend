b532a89f6c0b39122f093ecb9f310fe9cb61c49a
Kai Krings <kai.krings@posteo.de>
2020-02-29 21:43:32 -0600

    Update script for creating new diary entries.

--- bin/create.js
+++ bin/create.js
@@ -8,18 +8,19 @@ const Image = require('../models/image');
 
 /**
  * Connect to data base.
+ *
+ * @param {string} uri
+ *   MongoDB URI
  */
-async function connect() {
-  const mongooseUrl = 'mongodb://localhost:27017/weltenbummlerpaar';
-
+async function connect(uri) {
   const mongooseOptions = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   };
 
   try {
-    await mongoose.connect(mongooseUrl, mongooseOptions);
-    console.log(`Connected to ${mongooseUrl}.`);
+    await mongoose.connect(uri, mongooseOptions);
+    console.log(`Connected to ${uri}.`);
   } catch (err) {
     console.error('Cannot connect to database.');
     console.error(err);
@@ -29,13 +30,16 @@ async function connect() {
 /**
  * Save new diary entry to data base.
  *
- * @param {Object} entry Diary entry
+ * @param {string} uri
+ *   MongoDB URI
+ * @param {Object} entry
+ *   Diary entry
  */
-async function createEntry(entry) {
+async function createEntry(uri, entry) {
   const diaryEntry = new DiaryEntry(entry);
 
   try {
-    await connect();
+    await connect(uri);
     await diaryEntry.save();
     console.log('Diary entry has been saved.');
     await mongoose.connection.close();
@@ -48,13 +52,15 @@ async function createEntry(entry) {
 /**
  * Save new image to data base.
  *
+ * @param {string} uri
+ *   MongoDB URI
  * @param {Object} image Image
  */
-async function createImage(image) {
+async function createImage(uri, image) {
   const newImage = new Image(image);
 
   try {
-    await connect();
+    await connect(uri);
     await newImage.save();
     console.log('Image has been saved.');
     await mongoose.connection.close();
@@ -67,6 +73,12 @@ async function createImage(image) {
 // command-line arguments
 yargs
     .usage('Usage: $0 <command> [options]')
+    .options({
+      uri: {
+        describe: 'MongoDB URI',
+        default: 'mongodb://localhost:27017/weltenbummlerpaar',
+      },
+    })
     .command('entry [options]', 'Save new diary entry.', (yargs) => {
       yargs.options({
         title: {
@@ -77,17 +89,27 @@ yargs
           describe: 'Diary entry\'s body',
           demandOption: true,
         },
-        country: {
-          describe: 'Diary entry\'s country name',
+        locationName: {
+          describe: 'Diary entry\'s location name',
           demandOption: true,
         },
         images: {
           type: 'array',
           describe: 'Diary\'s image IDs',
         },
+        tags: {
+          type: 'array',
+          describe: 'Diary\'s search tags',
+        },
       });
-    }, (entry) => {
-      createEntry(entry);
+    }, (args) => {
+      createEntry(args.uri, {
+        title: args.title,
+        body: args.body,
+        locationName: args.locationName,
+        images: args.images,
+        tags: args.tags,
+      });
     })
     .command('image [options]', 'Save new image.', (yargs) => {
       yargs.options({
@@ -96,7 +118,9 @@ yargs
           demandOption: true,
         },
       });
-    }, (image) => {
-      createImage(image);
+    }, (args) => {
+      createImage(args.uri, {
+        description: args.description,
+      });
     })
     .argv;
