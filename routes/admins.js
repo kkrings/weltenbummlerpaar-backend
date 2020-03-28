84af7c6263653a6937dcd1ebed971dad4f6bb62f
Kai Krings <kai.krings@posteo.de>
2020-03-28 12:17:06 +0100

    Differentiate between failed login due to server error or wrong username and/or password.

--- routes/admins.js
+++ routes/admins.js
@@ -15,29 +15,24 @@ const router = new express.Router();
 
 router.use(bodyParser.json());
 
-/**
- * Send the admin user's JSON web token.
- *
- * @param {Object} req
- *   The request is expected to have the `user` property set, which holds the
- *   successfully authenticated admin user.
- * @param {Object} res
- *   The JSON response has the properties `success` and `token`. The former is
- *   a positive boolean that indicates that the admin user's authentication was
- *   successful. The latter holds the admin user's JSON web token.
- */
-function sendJwt(req, res) {
-  const token = jsonwebtoken.sign(
-      {_id: req.user._id}, config.jwtSecret, {expiresIn: 3600},
-  );
-
-  res.json({success: true, token: token});
-}
-
 // user login via username and password; signs a new JSON web token
-router.post('/login',
-    passport.authenticate('local', {session: false}),
-    sendJwt,
-);
+router.post('/login', function(req, res, next) {
+  passport.authenticate('local', function(err, user) {
+    if (err) {
+      next(err);
+    } else {
+      if (user) {
+        // sign and send new JSON web token
+        const token = jsonwebtoken.sign(
+            {_id: user._id}, config.jwtSecret, {expiresIn: 3600});
+
+        res.json({success: true, token: token});
+      } else {
+        // wrong username or password
+        res.status(403).json({success: false, token: ''});
+      }
+    }
+  })(req, res, next);
+});
 
 module.exports = router;
