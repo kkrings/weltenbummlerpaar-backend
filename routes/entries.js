a6d4226add8365ad9529a1d1424040f366b2d94e
Kai Krings <kai.krings@posteo.de>
2020-03-31 17:58:56 +0200

    Remove more unused routes; add a route for deleting a diary entry, including its images.

--- routes/entries.js
+++ routes/entries.js
@@ -1,8 +1,16 @@
+/**
+ * Diary entry routes
+ * @module routes/entries
+ */
+
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const authenticate = require('../authenticate');
 const DiaryEntry = require('../models/entry');
+const Image = require('../models/image');
+
 
 const router = new express.Router();
 
@@ -11,30 +19,19 @@ router.use(bodyParser.json());
 router.route('/')
     .get(async function(req, res, next) {
       try {
-        res.json(
-            await DiaryEntry.find(req.query.filter, null, req.query.options)
-                .populate('images')
-                .exec());
+        const findEntries = DiaryEntry.find(
+            req.query.filter, null, req.query.options);
+
+        res.json(await findEntries.populate('images').exec());
       } catch (err) {
         next(err);
       }
     })
     .post(authenticate.authorizeJwt, async function(req, res, next) {
       try {
-        let diaryEntry = new DiaryEntry(req.body);
-        diaryEntry = await diaryEntry.save();
-
-        res.json(
-            await DiaryEntry.findById(diaryEntry._id)
-                .populate('images')
-                .exec());
-      } catch (err) {
-        next(err);
-      }
-    })
-    .delete(authenticate.authorizeJwt, async function(_req, res, next) {
-      try {
-        res.json(await DiaryEntry.deleteMany({}).exec());
+        const diaryEntry = await (new DiaryEntry(req.body)).save();
+        const findEntry = DiaryEntry.findById(diaryEntry._id);
+        res.json(await findEntry.populate('images').exec());
       } catch (err) {
         next(err);
       }
@@ -43,31 +40,41 @@ router.route('/')
 router.route('/:entryId')
     .get(async function(req, res, next) {
       try {
-        res.json(
-            await DiaryEntry.findById(req.params.entryId)
-                .populate('images')
-                .exec());
+        const findEntry = DiaryEntry.findById(req.params.entryId);
+        res.json(await findEntry.populate('images').exec());
       } catch (err) {
         next(err);
       }
     })
     .put(authenticate.authorizeJwt, async function(req, res, next) {
       try {
-        res.json(
-            await DiaryEntry.findByIdAndUpdate(
-                req.params.entryId, {$set: req.body}, {new: true},
-            )
-                .populate('images')
-                .exec());
+        const updateEntry = DiaryEntry.findByIdAndUpdate(
+            req.params.entryId, {$set: req.body}, {new: true});
+
+        res.json(await updateEntry.populate('images').exec());
       } catch (err) {
         next(err);
       }
     })
     .delete(authenticate.authorizeJwt, async function(req, res, next) {
       try {
-        res.json(
-            await DiaryEntry.findByIdAndRemove(req.params.entryId)
-                .exec());
+        // remove diary entry
+        const diaryEntry = await DiaryEntry.findByIdAndRemove(
+            req.params.entryId).exec();
+
+        const deleteImages = Image.deleteMany({
+          _id: {$in: diaryEntry.images},
+        });
+
+        // remove diary entry's images from disk
+        deleteImages.cursor().on('data', function(image) {
+          fs.unlinkSync(`public/images/${image._id}.jpg`);
+        });
+
+        await deleteImages.exec();
+
+        // send deleted diary entry
+        res.json(diaryEntry);
       } catch (err) {
         next(err);
       }
