4575f2c41259c54bbb260d8731713ed051578c9f
Kai Krings <kai.krings@posteo.de>
2020-03-31 10:45:08 +0200

    Add new route for uploading images.

--- routes/images.js
+++ routes/images.js
@@ -1,7 +1,17 @@
+/**
+ * Image routes
+ * @module routes/images
+ */
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
 
+const authenticate = require('../authenticate');
 const Image = require('../models/image');
+
 
 const router = new express.Router();
 
@@ -26,5 +36,28 @@ router.route('/:imageId')
         next(err);
       }
     });
+
+const imageUpload = multer({dest: 'public/images/'});
+
+router.route('/upload')
+    .post(
+        authenticate.authorizeJwt,
+        imageUpload.single('image'),
+        async function(req, res, next) {
+          try {
+            const image = new Image(req.body);
+
+            // rename uploaded image given its ID
+            fs.renameSync(
+                req.file.path,
+                path.join(req.file.destination, `${image._id}.jpg`),
+            );
+
+            res.json(await image.save());
+          } catch (err) {
+            next(err);
+          }
+        },
+    );
 
 module.exports = router;
