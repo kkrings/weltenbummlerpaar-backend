5cef4f87d56e9ac6453749bcc8deda779b45d0d9
Kai Krings <kai.krings@posteo.de>
2020-03-31 17:58:17 +0200

    Remove unused routes.

--- routes/images.js
+++ routes/images.js
@@ -17,32 +17,10 @@ const router = new express.Router();
 
 router.use(bodyParser.json());
 
-router.route('/')
-    .get(async function(req, res, next) {
-      try {
-        res.json(
-            await Image.find(req.query.filter, null, req.query.options)
-                .exec());
-      } catch (err) {
-        next(err);
-      }
-    });
-
-router.route('/:imageId')
-    .get(async function(req, res, next) {
-      try {
-        res.json(await Image.findById(req.params.imageId).exec());
-      } catch (err) {
-        next(err);
-      }
-    });
-
 const imageUpload = multer({dest: 'public/images/'});
 
 router.route('/upload')
-    .post(
-        authenticate.authorizeJwt,
-        imageUpload.single('image'),
+    .post(authenticate.authorizeJwt, imageUpload.single('image'),
         async function(req, res, next) {
           try {
             const image = new Image(req.body);
@@ -50,14 +28,12 @@ router.route('/upload')
             // rename uploaded image given its ID
             fs.renameSync(
                 req.file.path,
-                path.join(req.file.destination, `${image._id}.jpg`),
-            );
+                path.join(req.file.destination, `${image._id}.jpg`));
 
             res.json(await image.save());
           } catch (err) {
             next(err);
           }
-        },
-    );
+        });
 
 module.exports = router;
