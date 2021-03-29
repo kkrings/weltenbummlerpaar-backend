753cb65fcc663c50361eebd79efde7e6a0b96131
Kai Krings <kai.krings@posteo.de>
2021-03-29 20:02:38 +0200

    Adjust config for Travis CI.

--- app.config.js
+++ app.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: 'weltenbummlerpaar-backend',
-    script: 'bin/weltenbummlerpaar-backend.js',
+    script: 'dist/index.js',
     instances: 1,
     autorestart: true,
     watch: false,
