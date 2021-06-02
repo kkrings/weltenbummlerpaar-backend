c9521990420553fb004ae19523d6fe816fa69432
Kai Krings <kai.krings@posteo.de>
2021-06-02 09:16:23 +0200

    Use diary entry schema for create diary entry DTO; add some descriptions.

--- src/diary-entries/dto/create-diary-entry.dto.ts
+++ src/diary-entries/dto/create-diary-entry.dto.ts
@@ -1,6 +1,8 @@
-export class CreateDiaryEntryDto {
-  title: string
-  location: string
-  body: string
+import { OmitType } from '@nestjs/swagger'
+import { DiaryEntry } from '../schema/diary-entry.schema'
+
+export class CreateDiaryEntryDto extends (
+  OmitType(DiaryEntry, ['id', 'searchTags', 'createdAt', 'updatedAt'] as const)
+) {
   searchTags: string[] = []
 }
--- src/diary-entries/schema/diary-entry.schema.ts
+++ src/diary-entries/schema/diary-entry.schema.ts
@@ -6,23 +6,49 @@ export type DiaryEntryDocument = DiaryEntry & mongoose.Document
 
 @Schema({ timestamps: true })
 export class DiaryEntry {
+  /**
+   * Diary entry's unique identifier
+   */
+  id: string
+
+  /**
+   * Diary entry's title
+   */
   @Prop({ required: true })
   title: string
 
+  /**
+   * Country, city, ..., the diary entry is about
+   */
   @Prop({ required: true })
   location: string
 
+  /**
+   * Diary entry's content
+   */
   @Prop({ required: true })
   body: string
 
+  /**
+   * Search tags the diary entry can be found with
+   */
   @Prop({
     type: [{
       type: mongoose.Schema.Types.ObjectId,
       ref: 'SearchTag'
-    }],
-    index: true
+    }]
   })
   searchTags: SearchTag[]
+
+  /**
+   * Diary entry's creation date-time
+   */
+  createdAt: Date
+
+  /**
+   * Date-time the diary entry was last modified
+   */
+  updatedAt: Date
 }
 
 export const DiaryEntrySchema = SchemaFactory.createForClass(DiaryEntry)
