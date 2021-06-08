836270293387fdffdc55ac7e9718ba6f9fb14db5
Kai Krings <kai.krings@posteo.de>
2021-06-08 20:56:14 +0000

    Remove search tags from create diary entry DTO.

--- src/diary-entries/dto/create-diary-entry.dto.ts
+++ src/diary-entries/dto/create-diary-entry.dto.ts
@@ -2,7 +2,5 @@ import { OmitType } from '@nestjs/swagger'
 import { DiaryEntry } from '../entities/diary-entry.entity'
 
 export class CreateDiaryEntryDto extends (
-  OmitType(DiaryEntry, ['_id', 'searchTags', 'createdAt', 'updatedAt'] as const)
-) {
-  searchTags: string[] = []
-}
+  OmitType(DiaryEntry, ['_id', 'createdAt', 'updatedAt'] as const)
+) {}
