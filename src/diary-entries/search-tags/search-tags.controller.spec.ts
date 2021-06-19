11902e6e59f684d9870fb073df767f59e0f782ee
Kai Krings <kai.krings@posteo.de>
2021-06-19 09:46:38 +0000

    Start implementing unit tests for search tags controller.

--- src/diary-entries/search-tags/search-tags.controller.spec.ts
+++ src/diary-entries/search-tags/search-tags.controller.spec.ts
@@ -3,12 +3,23 @@ import { SearchTagsController } from './search-tags.controller'
 import { SearchTagsService } from './search-tags.service'
 
 describe('SearchTagsController', () => {
+  const referenceSearchTags = ['some search tag']
+
+  const mockService = {
+    findSearchTags: async () => await Promise.resolve(referenceSearchTags)
+  }
+
   let controller: SearchTagsController
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SearchTagsController],
-      providers: [SearchTagsService]
+      providers: [
+        {
+          provide: SearchTagsService,
+          useValue: mockService
+        }
+      ]
     }).compile()
 
     controller = module.get<SearchTagsController>(SearchTagsController)
@@ -16,5 +27,23 @@ describe('SearchTagsController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined()
+  })
+
+  describe('findSearchTags()', () => {
+    const findSearchTagsSpy = jest.spyOn(mockService, 'findSearchTags')
+
+    let searchTags: string[]
+
+    beforeEach(async () => {
+      searchTags = await controller.findSearchTags()
+    })
+
+    it('should return reference search tags', () => {
+      expect(searchTags).toEqual(referenceSearchTags)
+    })
+
+    it('should have called service', () => {
+      expect(findSearchTagsSpy).toHaveBeenCalled()
+    })
   })
 })
