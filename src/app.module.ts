f6c4ccfd414bd031ed84278b1936354073e03475
Kai Krings <kai.krings@posteo.de>
2021-06-23 17:09:08 +0000

    Configure Mongoose via environment variables.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,16 @@ import { ConfigModule } from '@nestjs/config'
 import { MongooseModule } from '@nestjs/mongoose'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { DatabaseConfigService } from './database/database-config.service'
+import { DatabaseModule } from './database/database.module'
 import { DiaryEntriesModule } from './diary-entries/diary-entries.module'
-import imageUploadConfig from './diary-entries/images/image-upload/image-upload.config'
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      load: [imageUploadConfig]
-    }),
-    MongooseModule.forRoot('mongodb://localhost:27017/weltenbummlerpaar', {
-      useCreateIndex: true,
-      useFindAndModify: false
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [DatabaseModule],
+      useExisting: DatabaseConfigService
     }),
     DiaryEntriesModule
   ],
