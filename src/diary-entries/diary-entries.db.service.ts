35211d5bb85c16eabe95674509203989e69222bf
Kai Krings <kai.krings@posteo.de>
2021-07-08 19:17:00 +0000

    Make import relative.

--- src/diary-entries/diary-entries.db.service.ts
+++ src/diary-entries/diary-entries.db.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
 import { Model } from 'mongoose'
-import { throwOnNull } from 'src/schemas/base.schema'
+import { throwOnNull } from '../schemas/base.schema'
 import { CreateDiaryEntryDto } from './dto/create-diary-entry.dto'
 import { UpdateDiaryEntryDto } from './dto/update-diary-entry.dto'
 import { Image } from './images/schemas/image.schema'
