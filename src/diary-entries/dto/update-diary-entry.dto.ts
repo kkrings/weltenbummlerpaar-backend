8c922d73f73a57b7fc323ecd0e8b3cfc4408d95d
Kai Krings <kai.krings@posteo.de>
2021-07-25 09:41:11 +0000

    Add doc string to images property of UpdateDiaryEntryDto class.

--- src/diary-entries/dto/update-diary-entry.dto.ts
+++ src/diary-entries/dto/update-diary-entry.dto.ts
@@ -3,6 +3,10 @@ import { IsMongoId } from 'class-validator'
 import { CreateDiaryEntryDto } from './create-diary-entry.dto'
 
 export class UpdateDiaryEntryDto extends PartialType(CreateDiaryEntryDto) {
+  /**
+   * Change the ordering of the diary entry's images.
+   * @example ["60d468d1f33a8412d3cec16f", "60d468d1f33a8412d3cec16g"]
+   */
   @IsMongoId({ each: true })
   images?: string[]
 }
