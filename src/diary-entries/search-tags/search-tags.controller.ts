ba8eef78819afb9ccda5ea6afa70a2a44ef77922
Kai Krings <kai.krings@posteo.de>
2021-07-25 09:46:02 +0000

    Add a API tag to the search tags controller.

--- src/diary-entries/search-tags/search-tags.controller.ts
+++ src/diary-entries/search-tags/search-tags.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common'
+import { ApiTags } from '@nestjs/swagger'
 import { SearchTagsService } from './search-tags.service'
 
+@ApiTags('Diary entry search tags')
 @Controller('search-tags')
 export class SearchTagsController {
   constructor (private readonly searchTagsService: SearchTagsService) {}
