e94ee490c45e3ccdb6b5c725a2266b70c9ddeedd
Kai Krings <kai.krings@posteo.de>
2021-09-06 08:32:10 +0200

    Remove legacy code.

--- src/app/models/user.d.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose'
-
-declare global {
-  namespace Express {
-    // eslint-disable-next-line @typescript-eslint/no-empty-interface
-    interface User extends mongoose.PassportLocalModel<mongoose.PassportLocalDocument> { }
-  }
-}
