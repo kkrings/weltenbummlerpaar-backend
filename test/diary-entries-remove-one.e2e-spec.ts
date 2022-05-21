9ea7fe9a8bfd853668c972b0e44eff02148a18af
Kai Krings <kai.krings@posteo.de>
2022-05-21 16:18:07 +0000

    Add E2E tests for DiaryEntriesController.removeOne.

---
+++ test/diary-entries-remove-one.e2e-spec.ts
@@ -0,0 +1,81 @@
+import * as request from 'supertest';
+import {
+  DiaryEntryDto,
+  getDiaryEntries,
+} from '@kkrings/weltenbummlerpaar-e2e-data';
+import { INestApplication } from '@nestjs/common';
+import { login, setupApp, setupDB, TeardownDB } from './setup';
+
+describe('DiaryEntriesController.removeOne', () => {
+  let app: INestApplication;
+  let accessToken: string;
+  let teardownDB: TeardownDB;
+
+  beforeEach(async () => {
+    app = await setupApp();
+  });
+
+  beforeEach(async () => {
+    accessToken = await login(app);
+  });
+
+  beforeEach(async () => {
+    teardownDB = await setupDB(app);
+  });
+
+  describe('/{id} (DELETE)', () => {
+    let diaryEntry: DiaryEntryDto;
+    let response: request.Response;
+
+    beforeEach(() => {
+      diaryEntry = getDiaryEntries()[0];
+    });
+
+    beforeEach(async () => {
+      response = await request(app.getHttpServer())
+        .delete(`/diary-entries/${diaryEntry.id}`)
+        .set('Authorization', `Bearer ${accessToken}`)
+        .expect(200);
+    });
+
+    it('deleted diary entry should have been sent', () => {
+      expect(response.body).toEqual(diaryEntry);
+    });
+  });
+
+  describe('/{id} (DELETE); invalid diary entry ID', () => {
+    let response: request.Response;
+
+    beforeEach(async () => {
+      response = await request(app.getHttpServer())
+        .delete('/diary-entries/invalidmongoid')
+        .set('Authorization', `Bearer ${accessToken}`);
+    });
+
+    it('status code should be equal to 400', () => {
+      expect(response.statusCode).toEqual(400);
+    });
+  });
+
+  describe('/{id} (DELETE); non-existing diary entry', () => {
+    let response: request.Response;
+
+    beforeEach(async () => {
+      response = await request(app.getHttpServer())
+        .delete('/diary-entries/62890e001db53966f0c44142')
+        .set('Authorization', `Bearer ${accessToken}`);
+    });
+
+    it('status code should be equal to 404', () => {
+      expect(response.statusCode).toEqual(404);
+    });
+  });
+
+  afterEach(async () => {
+    await app.close();
+  });
+
+  afterEach(async () => {
+    await teardownDB();
+  });
+});
